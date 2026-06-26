ce7464dbfa166929dfa5a083cee03dda08669d6b
hollyandrex-cell <hollyandrex@gmail.com>
2026-06-26 17:09:44 +0200

    Add files via upload

--- bilancio/sw.js
+++ bilancio/sw.js
@@ -1,6 +1,14 @@
+const CACHE_NAME = 'hr-bilancio-v1.1'; // Cambiamo versione così si aggiorna
+
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open('hr-bilancio-v1').then((cache) => {
-    return cache.addAll(['./', './bilancio.html', './js/bilancio.js']);
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => {
+    return cache.addAll([
+      './', 
+      './bilancio.html', 
+      './js/bilancio.js',
+      './css/bilancio.css',    // ← Aggiunto: così lo stile resta figo offline
+      './manifest.json'       // ← Aggiunto: per installarla come app
+    ]);
   }));
 });
 
@@ -8,4 +16,16 @@ self.addEventListener('fetch', (e) => {
   e.respondWith(caches.match(e.request).then((response) => {
     return response || fetch(e.request);
   }));
+});
+
+// Bonus: pulisce le cache vecchie quando aggiorni
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(keys
+        .filter(key => key !== CACHE_NAME)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
 });
