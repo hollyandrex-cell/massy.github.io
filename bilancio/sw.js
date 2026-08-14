1a24384102f0a8e98119142a2f789cb526e5a080
hollyandrex-cell <hollyandrex@gmail.com>
2026-08-14 08:28:05 +0200

    Update sw.js

--- bilancio/sw.js
+++ bilancio/sw.js
@@ -1,31 +1,35 @@
-const CACHE_NAME = 'hr-bilancio-v1.1'; // Cambiamo versione così si aggiorna
+const CACHE_NAME = 'hr-bilancio-v1.2';
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => {
-    return cache.addAll([
-      './', 
-      './bilancio.html', 
-      './js/bilancio.js',
-      './css/bilancio.css',    // ← Aggiunto: così lo stile resta figo offline
-      './manifest.json'       // ← Aggiunto: per installarla come app
-    ]);
-  }));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        './',
+        './bilancio.html',
+        './manifest.json'
+        // Rimuoviamo js e css dalla cache iniziale - li carica da rete, così non fa errore se manca
+      ]).catch(err => {
+        console.log('Cache parziale:', err);
+        return cache.addAll(['./', './bilancio.html']);
+      });
+    })
+  );
+  self.skipWaiting();
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((response) => {
-    return response || fetch(e.request);
-  }));
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
 });
 
-// Bonus: pulisce le cache vecchie quando aggiorni
 self.addEventListener('activate', (e) => {
   e.waitUntil(
     caches.keys().then((keys) => {
-      return Promise.all(keys
-        .filter(key => key !== CACHE_NAME)
-        .map(key => caches.delete(key))
-      );
+      return Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)));
     })
   );
+  self.clients.claim();
 });
