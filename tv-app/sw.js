bbb9ef550196db1fb6e76800705b4063d4b18a46
hollyandrex-cell <hollyandrex@gmail.com>
2026-09-11 11:57:24 +0200

    Add files via upload

---
+++ tv-app/sw.js
@@ -0,0 +1,10 @@
+
+const CACHE='hrt-tv-app-v2';
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'])));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e=>{ e.waitUntil(self.clients.claim()); });
+self.addEventListener('fetch', e=>{
+  e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request).catch(()=>caches.match('./index.html'))));
+});
